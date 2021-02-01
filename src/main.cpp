5753e3f98fe9d41ce17d98bf66d1692f48c9870f
Eshan Chatterjee <eshan2001221@gmail.com>
2021-02-02 02:08:35 +0530

    main = test file

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,20 @@
 #include "fmt/color.h"
 #include "fmt/core.h"
-#include "fmt/color.h"
-#include <iostream>
 #include "SystemInfo.h"
 
 int main(int argc, char** argv)
 {
-	fmt::print(fg(fmt::color::deep_pink) | fmt::emphasis::bold, "Daletos [Starting.....]\n");
+	sys Obj;
+	fmt::print("\n**********************************************************************\n");
+	fmt::print(fg(fmt::color::deep_pink) | bg(fmt::color::gainsboro) | fmt::emphasis::bold, "Daletos [Starting.....]\n");
+	fmt::print("\n**********************************************************************\n");
+	Sleep(700);
 	system("cls");
-	fmt::print(fg(fmt::color::deep_pink) | bg(fmt::color::gainsboro) | fmt::emphasis::bold, "Daletos");
+	fmt::print("\n**********************************************************************\n");
+	fmt::print(fg(fmt::color::deep_pink) | bg(fmt::color::gainsboro) | fmt::emphasis::bold, "Daletos\t");
 	fmt::print(fg(fmt::color::green) | fmt::emphasis::bold, "[Status: Online]\n");
-
-	std::string m_cwd = get_current_dir();
-	fmt::print("Current Directory:\t {} \n", get_current_dir());
+	fmt::print("Current Directory:\t {} \n", Obj.returnCWD());
+	fmt::print(fmt::emphasis::bold, "© Copyright Eshanatnite");
+	fmt::print("\n**********************************************************************");
 	std::cin.get();
 }
