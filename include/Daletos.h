bf67c29399f5fc415f43f2439994606450f0a7eb
Eshan Chatterjee <eshan2001221@gmail.com>
2021-02-02 14:38:32 +0530

    Update app class

--- include/Daletos.h
+++ include/Daletos.h
@@ -1,13 +1,25 @@
 #pragma once
 #include "SystemInfo.h"
 #include <array>
+#include <vector>
+#include <filesystem>
 
 class Daletos
 {
 private:
 	sys m_systemObject;
-	std::array<std::string, 10> m_argv; // Todo
+	// ! std::array m_argv might be useless
+	// ? Maybe depricated
+	std::array<std::string, 7> m_argv {"-h", "--help", "-s", "--search", "-fs", "--fast-search", "search"};
+	std::vector<std::string> m_files;
 
 public:
 	Daletos();
+	Daletos(Daletos& other) = delete;
+	Daletos(Daletos&& other) = delete;
+
+	void start(int argc, char** argv);
+
+private:
+	void search_fs(const std::string& fileName);
 };
--- src/Daletos.cpp
+++ src/Daletos.cpp
@@ -4,8 +4,10 @@
 #include <windows.h>
 #include <iostream>
 
+
 Daletos::Daletos()
 {
+	m_files.resize(10);
 	for (int i = 0; i <= 70; i++)
 	{
 		fmt::print("*");
@@ -29,7 +31,7 @@ Daletos::Daletos()
 	fmt::print(fg(fmt::color::deep_pink) | bg(fmt::color::gainsboro) | fmt::emphasis::bold, "**\tDaletos\t");
 	fmt::print(fg(fmt::color::green) | fmt::emphasis::bold, "[Status: Online]\n");
 	fmt::print("Current Directory:\t {} \n", m_systemObject.m_cwd);
-	fmt::print(fmt::emphasis::bold, "© Copyright Eshanatnite");
+	fmt::print(fmt::emphasis::bold, "ï¿½ Copyright Eshanatnite");
 
 	for (int i = 0; i <= 70; i++)
 	{
@@ -41,4 +43,37 @@ Daletos::Daletos()
 	fmt::print(fg(fmt::color::aquamarine) | fmt::emphasis::italic, "Ram:\t{}", m_systemObject.m_ram);
 
 	std::cout << std::endl;
+}
+
+// Todo :: incomplete
+void Daletos::start(int argc, char** argv)
+{
+	if(argc < 1)
+	{
+		if (argv[1] == "-h" || argv[1] == "--help")
+		{
+			fmt::print(fg(fmt::color::gainsboro) | fmt::emphasis::italic, "-h, --help -->\t\t");
+			fmt::print(fmt::emphasis::bold, "Helps Provides documentation and help tips\n");
+			fmt::print(fg(fmt::color::cyan) | fmt::emphasis::bold, "search -->\n");
+			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "-s, --search\t");
+			fmt::print(fmt::emphasis::bold, "Normal Search algorithm (Set By Default)\n");
+			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "-fs, --fast-search\t");
+			fmt::print(fmt::emphasis::bold, "Fast Search algorithm\n");
+		}
+
+		if(argc > 2)  // Todo : Incomplete
+		{
+			if(argv[2] == "search")
+			{}
+		}
+	}
+}
+
+// ?? does this even work ??
+void Daletos::search_fs(const std::string& fileName)
+{
+	for(const auto&entry : std::filesystem::directory_iterator(m_systemObject.m_cwd))
+	{
+		m_files.emplace_back(entry.path().string());
+	}
 }
