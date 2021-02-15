eeb0710681dee8feed8a25604072e39f256e3d87
Eshan Chatterjee <eshan2001221@gmail.com>
2021-02-16 03:18:52 +0530

    Dalatos update

--- include/Daletos.h
+++ include/Daletos.h
@@ -1,17 +1,14 @@
 #pragma once
 #include "SystemInfo.h"
-#include <array>
 #include <vector>
-#include <filesystem>
+#include <dirent.h>
+#include <windows.h>
 
 class Daletos
 {
 private:
 	sys m_systemObject;
 	std::string m_file;
-	// ! std::array m_argv might be useless
-	// ? Maybe depricated
-	std::array<std::string, 7> m_argv {"-h", "--help", "-s", "--search", "-fs", "--fast-search", "search"};
 	std::vector<std::string> m_files;
 
 public:
@@ -22,5 +19,10 @@ public:
 	void start(int argc, char** argv);
 
 private:
-	void search_fs(const std::string& fileName);
+	inline void vecSort();
+	void collect_fs();
+	void collect_dirent();
+	void collect_WIN();
+
+	void find();
 };
--- src/Daletos.cpp
+++ src/Daletos.cpp
@@ -1,13 +1,17 @@
 #include "Daletos.h"
 #include "fmt/color.h"
 #include "fmt/core.h"
+#include "fmt/ostream.h"
+#include "str_manip.h"
 #include <windows.h>
 #include <cstring>
 #include <iostream>
-
+#include <utility>
+#include <filesystem>
+#include <algorithm>
 
 Daletos::Daletos(int argc, char** argv)
-:	m_file(argv[3])
+	: m_file(argv[3])
 {
 	m_files.resize(10);
 	for (int i = 0; i <= 70; i++)
@@ -44,52 +48,114 @@ Daletos::Daletos(int argc, char** argv)
 	fmt::print(fg(fmt::color::aquamarine) | fmt::emphasis::italic, "Number of Cores:\t{}\n", m_systemObject.m_ram);
 	fmt::print(fg(fmt::color::aquamarine) | fmt::emphasis::italic, "Ram:\t{}", m_systemObject.m_ram);
 
-	std::cout << std::endl;
+	std::cout << std::flush;
 }
 
 // Todo :: incomplete
 void Daletos::start(int argc, char** argv)
 {
-	if(argc < 2)
+	if (argc < 2)
 	{
 		if (std::strcmp(argv[1], "-h") || std::strcmp(argv[1], "--help"))
 		{
 			fmt::print(fg(fmt::color::gainsboro) | fmt::emphasis::italic, "-h, --help -->\t\t");
 			fmt::print(fmt::emphasis::bold, "Helps Provides documentation and help tips\n");
 			fmt::print(fg(fmt::color::cyan) | fmt::emphasis::bold, "search -->\n");
-			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "-s, --search\t");
+			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "search\t");
 			fmt::print(fmt::emphasis::bold, "Normal Search algorithm (Set By Default)\n");
-			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "-fs, --fast-search\t");
+			fmt::print(fg(fmt::color::alice_blue) | fmt::emphasis::bold, "--fast-search\t");
 			fmt::print(fmt::emphasis::bold, "Fast Search algorithm\n");
 		}
 
-		else if(argc > 2)  // Todo : Incomplete
+		else if (argc > 2)  // Todo : Incomplete
 		{
-			if(std::string(argv[2]).rfind(".\\", 0) == 0 )
-				m_systemObject.m_searchDir = std::string(argv[2]);
-			if())
-			if(std::strcmp(argv[3], "-s") || std::strcmp(argv[3], "search"))
-			{}
+			if (!std::strcmp(argv[2], "search"))
+			{
+				// SettingUp Path variables
+				if (std::string(argv[3]).rfind(".\\", 0) == 0)
+				{
+					m_systemObject.m_searchDir = std::string(argv[3]);
+					m_file = std::move(argv[4]);
+					// check fast search
+					if (!std::strcmp(argv[5], "--fast_search") || !std::strcmp(argv[5], "-fs"))
+					{
+						collect_dirent();
+					}
+				}
+
+				else
+				{
+					m_systemObject.m_searchDir = std::move (m_systemObject.m_cwd);
+					m_file = std::move(argv[3]);
+				}
+			}
 		}
 	}
 }
 
+/* Sorts the Files Vector */
+inline void Daletos::vecSort()
+{
+	std::sort(m_files.begin(), m_files.end());
+}
+
 
 /* Adds Files via the FileSystem Header */
-void Daletos::search_fs(const std::string& fileName)
+void Daletos::collect_fs()
 {
-	for(const auto&entry : std::filesystem::directory_iterator(m_systemObject.m_cwd))
+	for (const auto& entry : std::filesystem::directory_iterator(m_systemObject.m_searchDir))
 	{
 		m_files.emplace_back(entry.path().string());
 	}
 
-	for(const auto&entry : m_files)
-	{
-		if(entry == m_file)
-			fmt::print(fg(fmt::color::lime_green) | fmt::emphasis::bold, "File Found in the Current Directory\n")
-		else
-		{
+	vecSort();
+}
 
-		}
+/* Adds Files via the FileSystem Header */
+void Daletos::collect_dirent()
+{
+	DIR *dir;
+    dir = opendir(m_systemObject.m_searchDir.c_str());
+    if (dir == NULL)
+		fmt::print(std::cerr, fg(fmt::color::red) | fmt::emphasis::bold, "Error in Opening Directory\n");
+
+    // get file names
+    struct dirent *ent;
+    while ((ent = readdir(dir)) != NULL)
+        m_files.push_back(ent->d_name);
+    closedir(dir);
+
+    // delete current and parent directories
+    m_files.erase(std::find(m_files.begin(), m_files.end(), "."));
+    m_files.erase(std::find(m_files.begin(), m_files.end(), ".."));
+
+	vecSort();
+}
+
+/* Adds files using the Windows API */
+void Daletos::collect_WIN()
+{
+	WIN32_FIND_DATA FindFileData;
+	wchar_t* FileName = string2wchar_t(m_systemObject.m_searchDir);
+	HANDLE hFind = FindFirstFile(FileName, &FindFileData);
+
+	m_files.push_back(wchar_t2string(FindFileData.cFileName));
+
+	while (FindNextFile(hFind, &FindFileData))
+		m_files.push_back(wchar_t2string(FindFileData.cFileName));
+
+	vecSort();
+}
+
+
+void Daletos::find()
+{
+	std::vector<std::string>::iterator flag = std::find(m_files.begin(), m_files.end(), m_file);
+	if(flag != std::end(m_files))
+		fmt::print(fg(fmt::color::aquamarine), "FILE Found in the Working Directory\n");
+
+	else
+	{
+		fmt::print(fg(fmt::color::red) | fmt::emphasis::bold, "FILE not found in the Working Directory\n");
 	}
 }
