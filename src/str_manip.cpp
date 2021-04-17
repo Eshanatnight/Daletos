e6fb5802796c448d406c0b1b8bf55647b9e195b8
Eshan Chatterjee <eshan2001221@gmail.com>
2021-04-18 01:18:55 +0530

    update str_manip

--- src/str_manip.cpp
+++ src/str_manip.cpp
@@ -1,5 +1,6 @@
 #include "str_manip.h"
 
+/* checks weather a string ends with a specific substr or not (abstracted) */
 inline bool ends_with(std::string const& value, std::string const& ending)
 {
 	if (ending.size() > value.size())
@@ -8,6 +9,7 @@ inline bool ends_with(std::string const& value, std::string const& ending)
 	return std::equal(ending.rbegin(), ending.rend(), value.rbegin());
 }
 
+/* Converts a wchar_t to a string */
 std::string wchar_t2string(const wchar_t* wchar)
 {
 	std::string str = "";
@@ -20,6 +22,7 @@ std::string wchar_t2string(const wchar_t* wchar)
 	return str;
 }
 
+/* Converts a string to a wchar_t */
 wchar_t* string2wchar_t(const std::string& str)
 {
 	wchar_t wchar[260];
@@ -31,4 +34,17 @@ wchar_t* string2wchar_t(const std::string& str)
 	}
 	wchar[index] = 0;
 	return wchar;
+}
+
+/* Checks weather a container with strings is empty or not */
+bool isEmpty(const std::vector<std::string>& container)
+{
+	bool check = true;
+	for (const std::string& elem : container)
+	{
+		if (!elem.empty())
+			check = false;
+		break;
+	}
+	return check;
 }
